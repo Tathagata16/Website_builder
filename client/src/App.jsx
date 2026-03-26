c5aa6a1dece136c44b1d3e76b85c6c2adaa3a55b
Tathagata16 <tathagataghosh1609@gmail.com>
2026-03-26 22:08:12 +0530

    connecting backend api with axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 
+export const serverUrl = "http://localhost:5000";
+
 function App() {
   return (
     <BrowserRouter>
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,7 +23,7 @@ try {
 
     return res.status(200).json(user)
 } catch (error) {
-    
+    // console.log(error);
     return res.status(500).json({message:`google auth error ${error}`})
 }
 }
