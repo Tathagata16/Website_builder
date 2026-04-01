999ee6f25f90744ce405bb60bd50a57b7e4e269e
Tathagata16 <tathagataghosh1609@gmail.com>
2026-04-02 01:46:00 +0530

    Creating the frontend

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { useEffect } from 'react'
 import {serverUrl} from '../App.jsx'
 import axios from 'axios'
+import {useDispatch} from 'react-redux'
+import { setUserData } from '../redux/userSlice.js'
 
 function useGetCurrentUser() {
+  const dispatch  = useDispatch();
   useEffect(()=>{
     const getCurrentUser = async ()=>{
         try{
             const result= await axios.get(`${serverUrl}/api/user/me`, {withCredentials: true})
-
-            console.log(result);
+            dispatch(setUserData(result.data))
+            
         }catch(error){
             console.log(error);
         }
